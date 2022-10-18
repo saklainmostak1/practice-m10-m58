4bc7def634564bb9d24b45158a1c5470d4d7cff5
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-19 00:25:37 +0600

    login

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxYprt-tuO-87ztN7MHXTzD_9IGOziwuk",
+  authDomain: "practice-recap-auth.firebaseapp.com",
+  projectId: "practice-recap-auth",
+  storageBucket: "practice-recap-auth.appspot.com",
+  messagingSenderId: "538451018399",
+  appId: "1:538451018399:web:c8faecb8ebf0613de4d8aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
